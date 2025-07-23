657038c639b3e96457c0d921549a6aabf8069847
Ha Tran <hatran.hattt@gmail.com>
2025-07-23 22:59:38 +0700

    (Update homepage)

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+// --------------- Main Process - START --------------------------
+let challengeContainer = document.querySelector('.challenge-wrapper');
+let dataChallenges = '';
+
+
+// Load activities data from json file, then update view
+fetch('./data-challenges.json').then((res) => {
+  if (!res.ok) return console.log('Something went wrong.')
+
+  return res.json();
+}).then((data) => {
+  dataChallenges = data;
+  updateChallengeContainer(0);
+});
+
+// --------------- Main Process - END --------------------------
+
+// --- Update Challenges view
+function updateChallengeContainer() {
+  if (!dataChallenges) return console.log('Something went wrong.');
+
+  dataChallenges.forEach((item) => {
+
+    const section = document.createElement('section');
+
+    const header = document.createElement('h2')
+    header.innerHTML = item.category;
+    section.appendChild(header);
+
+    const listChallenges = document.createElement('ul');
+    item.challenges.forEach(challenge => {
+      const challengeElement = document.createElement('li');
+      challengeElement.innerHTML = `
+        <a href="./${item.category}/${challenge}/index.html">${challenge}</a>
+      `;
+
+      listChallenges.appendChild(challengeElement);
+    });
+    section.appendChild(listChallenges);
+
+
+    challengeContainer.appendChild(section);
+  });
+
+}
